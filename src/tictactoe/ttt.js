2635948ca2474cf81f90524b02c0c13e7e74e8ae
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-02-26 22:53:43 -0600

    i tried

---
+++ src/tictactoe/ttt.js
@@ -0,0 +1,162 @@
+// get an element from the DOM
+// var mainContainer = document.getElementsByClassName('.mainContainer')
+// var mainContainerQuery = document.querySelector('.mainContainer')
+// console.log('.mainContainer', mainContainer)
+// console.log('.mainContainerQuery', mainContainerQuery)
+// var body = document.querySelector('body')
+// body.style.margin = 0;
+// mainContainerQuery.style.backgroundColor = 'green'
+// mainContainer.style.height = '100%';
+// mainContainer[0].style.width = '100%';
+// mainContainerQuery[0].style.display = 'flex'
+// mainContainerQuery[0].style.justifyContent = 'center'
+// mainContainerQuery.style.alignItems = 'center'
+
+// // create Element
+// var board = document.createElement('div')
+// mainContainerQuery.appendChild(board)
+
+// // create a class for tic tac toe board
+// board.className = 'board'
+// // add style
+// board.style.width = '50%'
+// board.style.height = '50%'
+// board.style.border = '2px solid black'
+
+// // create a function
+// function backgroundRed (borderBox) {
+// console.log('borderBox', borderBox)
+// return borderBox.innerText = 'Don\'t click me'
+// // mainContainerQuery.style.backgroundColor = 'red'
+// mainContainerQuery.classList.toggle('red');
+
+//https://www.youtube.com/watch?v=yaPUl31nypk
+
+
+const winCombos = [
+  // these are the locations on the board as seen by their ID
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+const grid = () => Array.from(document.getElementsByClassName('clickBox'));
+
+const board = (clickBoxE1) => Number.parseInt(clickBoxE1.id.replace('l',''));
+
+const emptyBoard = () => grid().filter(_clickBoxE1 => _clickBoxE1.innerText === '');
+
+const winner = (arr) => arr.every(_clickBoxE1 => _clickBoxE1.innerText === arr[0].innerText && _clickBoxE1.innerText !== '');
+
+const yourTurn = (index, letter) => document.getElementsByClassName('.board'[index].innerText) = letter;
+
+const theirChoice = () => board(emptyBoard()[Math.floor(Math.random() * emptyBoard().length)]);
+
+const theirTurn =  () => {
+  disableListeners();
+  setTimeout(() => {
+    yourTurn(theirTurn(), 'O');
+  }, 1000);
+};
+
+const clickFn = ($event) => {
+  yourTurn(board($event.target), 'X');
+  theirTurn();
+};
+
+const enableListeners = () => grid().forEach(_clickBoxE1 => _clickBoxE1.addEventListener('click', clickFn));
+const disableListeners = () => grid().forEach(_clickBoxE1 => _clickBoxE1.removeEventListener('click', clickFn));
+
+enableListeners();
+
+
+// }
+
+// var previousPlay = null
+// function addGamePiece (selectedElement) {
+// var newElement = document.createElement('h1')
+
+// previousPlay = setGamePiece()
+// newElement.innerText = previousPlay
+// selectedElement.appendChild(newElement)
+// }
+
+// function setGamePiece() {
+// if (previousPlay === 'X') {
+// return 'O'
+// } else if (previousPlay === 'O') {
+// return 'X'
+// } else {
+// return 'X'
+// }
+// }
+
+
+// // const playerO = 'O';
+// // const playerX = 'X';
+
+// // this will be the winning standard
+// const winCombos = [
+//   // these are the locations on the board as seen by their ID
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [6, 4, 2],
+// ]
+
+// const box = document.querySelectorAll('.box');
+// play();
+
+// function play() {
+//   document.querySelector(".endgame").style.display = "none";
+//   origBoard = Array.from(Array(9).keys());
+//   for (var i = 0; i < cell.length; i++) {
+//     box[i].innerText = '';
+//     box[i].style.removeProperty('background-color');
+//     box[i].addEventListener['click', turnClick, false];
+//   }
+// }
+
+// function turnClick(location) {
+//  turn(location.target.id, playerO)
+// }
+
+// // function turn(location, players) {
+// //   origBoard[locationId] = players;
+// //   document.getElementById(locationId).innerText = players;
+// //   let gameWon = checkWin(origBoard, players) 
+// //   if (gameWon) {
+// //    return gameOver(gameWon)
+// //   }
+
+// // function checkWin(board, players) {
+// //   let plays = boards.reduce((a, e, i) >= (e === players)) ? (a.concat(i) : a, []); 
+// // }
+
+// //   let gameWon = null;
+// //   for (let [index, win] of winCombos.entries()) {
+// //     if (win.every(elem => plays.indexOf(elem > -1))) {
+// //       gameWon = {index: index, players: players};
+// //       break
+// //     }
+// //   }
+// //   return gameWon;
+// // }
+
+// // function gaveOver(gameWon) {
+// //   for (let index of winCombos[gameWon.index]) {
+// //     document.getElementById(index).style.backgroundColor = gameWon.player == playerX ? "purple" : "yellow";
+// //   }
+// //   for (var i = 0; i < cell.length; i++) {
+// //     cells[i].removeElementListener('click', turnClick, false)
+// //   }
+// // }
