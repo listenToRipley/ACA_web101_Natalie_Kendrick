491b7aa040ee0f78df19fc6504afb6c6d0168596
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-02-29 00:33:18 -0600

    updated values and functions, tryed some jquery options

--- src/todo+tower/tower/tower.js
+++ src/todo+tower/tower/tower.js
@@ -1,31 +1,54 @@
 // FYI, this is built with jQuery
 'use strict';
 
-var blue = ($('[data-row]').hasClass('blue') === $('[data-color]').hasClass('blue')); 
-var green = ($('[data-row]').hasClass('green') === $('[data-color]').hasClass('green'));
-var red = ($('[data-row]').hasClass('red') === $('[data-color]').hasClass('red'));
-var yellow = ($('[data-row]').hasClass('yellow') === $('[data-color]').hasClass('yellow')); 
+var blue = ($('.dataRow').hasClass('blue') && $('.dataColor').hasClass('blue')); 
+
+var green = ($('.dataRow').hasClass('green') && $('.dataColor').hasClass('green'));
+
+var red = ($('.dataRow').hasClass('red') && $('.dataColor').hasClass('red'));
+
+var yellow = ($('.dataRow').hasClass('yellow') && $('.dataColor').hasClass('yellow')); 
 
 $(document).ready(function() {
   let $block = null;
   
-  $('[data-row]').click(function() {
+  var move = ($('.dataRow').click(function() {
     if ($block) {
       $(this).append($block);
       $block = null;
-    } if (blue = true){
-      $block = $(this).children().last().detach();
-    } if (green = true) {
-      $block = $(this).children().last().detach();
-    } if (red = true) {
-      $block = $(this).children().last().detach();
-    } if (yellow = true) {
-      $block = $(this).children().last().detach();
-    } else {
+    } else { 
+      $block = $(this).children().last().detach();}
+     } ));
+    // if false what should happened
+
+  if (blue === true){
+    return move || null;
+  } else if (green === true) {
+    $block = $(this).children().last().detach();
+  } else if (red === true) {
+    $block = $(this).children().last().detach();
+  } else if (yellow === true) {
+    $block = $(this).children().last().detach();
+  } 
+
+    else if (blue !== true){
+    $block = $(this).detach('click', function (event) {
+      $('#blue').error(function()
+      {
+       alert('this is row is not blue, try again')
+     })
+    });
+  } else if (green !== true) {
+    $block = $(this).children().last().detach();
+  } else if (red !== true) {
+    $block = $(this).children().last().detach();
+  } else if (yellow !== true) {
+    $block = $(this).children().last().detach();
+  }
+    else {
       $block = null;
     }
-  })
-});
+  }); 
 
 // $(document).ready(function() {
 // var dataRow = $('div');
