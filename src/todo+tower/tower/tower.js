125fa0129a8081be0f32fe6b2fdf0c8d7b3c56dc
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-03-01 19:39:48 -0600

    tower: tried to recreate better rules. still not working

--- src/todo+tower/tower/tower.js
+++ src/todo+tower/tower/tower.js
@@ -1,59 +1,58 @@
 // FYI, this is built with jQuery
 'use strict';
 $(document).ready(function() {
-  var blue = ($('.dataRow').hasClass('blue') && $('.dataColor').hasClass('blue')); 
 
-  var green = ($('.dataRow').hasClass('green') && $('.dataColor').hasClass('green'));
+  let $main = document.getElementById('#mainContainer')
 
-  var red = ($('.dataRow').hasClass('red') && $('.dataColor').hasClass('red'));
+  // items
 
-  var yellow = ($('.dataRow').hasClass('yellow') && $('.dataColor').hasClass('yellow')); 
+  let dataColor = document.getElementsByClassName('dataColor');
+  let dataRow = document.getElementsByClassName('dataRow');
+
+  // squares
+
+  let bluSq = document.getElementById('blue');
+  let greSq = document.getElementById('green');
+  let redSq = document.getElementById('red');
+  let yelSq = document.getElementById('yellow');
+
+  // row
+
+  let bluRow = document.getElementById('drBlu');
+  let greRow = document.getElementById('drGre');
+  let redRow = document.getElementById('drRed');
+  let yelRow = document.getElementById('drYel');
+  let rowHolder = document.getElementById('drHol')
+
+  // moving the squares 
 
   let $block = null;
-  
-  var move = ($('.dataRow').click(function() {
+
+  ($('.dataRow').click(function() {
     if ($block) {
       $(this).append($block);
       $block = null;
     } else { 
       $block = $(this).children().last().detach();}
      } ));
-    // if false what should happened
 
-  if (blue === true){
-    return move || null;
-  } else if (green === true) {
-    $block = $(this).children().last().detach();
-  } else if (red === true) {
-    $block = $(this).children().last().detach();
-  } else if (yellow === true) {
-    $block = $(this).children().last().detach();
-  } 
+  function tower(row, square) {
+      if (bluRow === bluSq) {
+      after().detach().alert('blue is true! great job!');
+    } else if (greRow === greSq)  {
+      addEventListener.on(move).after().alert('It\'s easy to be green sometimes! These match')
+    } else if (redRow === redSq)  {
+      addEventListener.on(move).after().alert('Red Rum! These match')
+    } 
+      else if (yelRow === yelSq) {
+        addEventListener.on(move).after().alert('Most definately mellow! These match')
+      }
 
-    else if (blue !== true){
-    $block = $(this).detach('click', function (event) {
-      $('#blue').error(function()
-      {
-       alert('this is row is not blue, try again')
-     })
-    });
-  } else if (green !== true) {
-    $block = $(this).children().last().detach();
-  } else if (red !== true) {
-    $block = $(this).children().last().detach();
-  } else if (yellow !== true) {
-    $block = $(this).children().last().detach();
+    // tower(bluRow,bluSq);
+    // tower(greRow,greSq);
+    // tower(redRow,redSq);
+    // tower(yelRow,yelSq); 
   }
-    else {
-      $block = null;
-    }
-  }); 
+});
 
-// $(document).ready(function() {
-// var dataRow = $('div');
-// var dataColor = $('div');
 
-// if (dataRow.length == dataColor.length && dataRow.length == dataRow.filter(dataColor).length) {         
-
-// }
-// });
